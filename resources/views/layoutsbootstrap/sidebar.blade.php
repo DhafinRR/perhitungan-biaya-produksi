af581914285ae5c38eec11c800f821b476981ca2
DhafinRR <dhafinrajabi@gmail.com>
2024-05-06 20:55:09 +0700

    change sidebar

--- resources/views/layoutsbootstrap/sidebar.blade.php
+++ resources/views/layoutsbootstrap/sidebar.blade.php
@@ -137,15 +137,6 @@
                 <span class="hide-menu">Login</span>
               </a>
             </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="{{ url('penjualan') }}" aria-expanded="false">
-                <span>
-                  <i class="ti ti-wallet"></i>
-                </span>
-                <span class="hide-menu">Penjualan</span>
-              </a>
-            </li>
-          
             <li class="nav-small-cap">
               <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
               <span class="hide-menu">LAPORAN</span>
