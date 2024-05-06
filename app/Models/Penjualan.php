ae63f5765c52701d08a626ab5955553061b65a95
farhanmd7 <161566083+farhanmd7@users.noreply.github.com>
2024-05-06 14:09:31 +0700

    fix

--- app/Models/Penjualan.php
+++ app/Models/Penjualan.php
@@ -4,11 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-<<<<<<< HEAD
 // jika ingin menggunakan query biasa
 use Illuminate\Support\Facades\DB;
-=======
->>>>>>> 30362655de8032a759cf1eae6dbf8474f0535711
 
 class Penjualan extends Model
 {
